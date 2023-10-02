49c3b9b712603adeeac8d54f1e0361bf94f1d85e
walter1912 <thaiho2k@gmail.com>
2023-10-02 09:59:05 +0700

    update bug hover

--- src/modules/products/templates/detail-products/components/frame-pictures/SlideImage.tsx
+++ src/modules/products/templates/detail-products/components/frame-pictures/SlideImage.tsx
@@ -48,12 +48,14 @@ function SlideImage({
           position: "absolute",
           top: "50%",
           right: 0,
-          left: 0,
+          left: -16,
+        
           transform: "translate(0, -50%)",
           display: "flex",
           justifyContent: "space-between",
           alignItems: "center",
           svg: {
+            cursor:'pointer',
             fill: "var(--black)",
             fontSize: "36px",
             border: " 1px solid var(--blue)",
@@ -65,9 +67,9 @@ function SlideImage({
     >
       <div className="nav-btn">
         <ArrowLeft onClick={leftData} />
+      {children}
         <ArrowRight onClick={nextData} />
       </div>
-      {children}
     </Box>
   );
 }
