22466971e8769c18ac7f1090f8bad5f77e4068df
walter1912 <thaiho2k@gmail.com>
2023-11-13 03:05:01 +0700

    fix again

--- src/assets/fake-data/productcart.ts
+++ src/assets/fake-data/productcart.ts
@@ -5,11 +5,11 @@ import { ProductCart } from "~/models/productCart";
 export let prod: Product = { ...product };
 prod.size = 36;
 export let productCart: ProductCart = {
-  id: "12",
+  _id: "12",
   product: prod,
   quantity: 3,
   coupon: "",
   price: 90000,
 };
 export let productCart2 = { ...productCart };
-productCart2.id = "13";
+productCart2._id = "13";
